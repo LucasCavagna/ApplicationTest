1b81d9feef0692460abacc3b8a61c6fc91135dcc
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-03-27 16:29:16 -0300

    [Stage 1]
    Inicio de la aplicación

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace cursoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace cursoApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
